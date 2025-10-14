23c7c923424eb48a95773932fe25230f4341fe3f
0702849 <0702849@spsd.sk.ca>
2025-10-14 09:43:55 -0600

    rectangle rotates funny in model test

--- model-test/sketch.js
+++ model-test/sketch.js
@@ -1,12 +1,21 @@
 // Camera / 3d model test
 // Rylan Hamel Chan
 
-
+let angle = 0;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 300, WEBGL);
 }
 
 function draw() {
-  background(220);
+
+  background(175);
+
+  rectMode(CENTER)
+  noStroke();
+  fill(0,0,255);
+  rotateX(angle);
+  rect(0, 0, 150, 100);
+
+   angle += 0.07;
 }
