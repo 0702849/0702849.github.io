122cb459466717e37503c460567fc3e966e1cf4e
0702849 <0702849@spsd.sk.ca>
2025-10-17 09:52:25 -0600

    spawn in the box looks better and more like a simulator, love how its turning out need to figure out how to stop wasd controls

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -13,14 +13,19 @@ function setup() {
   rover = createRoverCam();
   rover.usePointerLock();    // optional; default is keyboard control only
   rover.setState({           // optional
-    position: [-400,-200,-200],
+    position: [0,0,0],
     rotation: [0.4,0.3,0],
     sensitivity: 0.01,
+    fov: 2,
     speed: 0.5
   });
 }
 
 function draw() {
   background(0);
-  box(200);
+  fill(140);
+  box(500);
+
+  // -- crosshair --
+  circle(width/2, height/2, 4);
 }
