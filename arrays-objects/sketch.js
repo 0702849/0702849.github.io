bc82831882ec24d5d5cd041b02b80452d7885205
0702849 <0702849@spsd.sk.ca>
2025-10-17 09:32:25 -0600

    working on project

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,11 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+var rover;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  rover = createRoverCam();
+  rover.usePointerLock();    // optional; default is keyboard control only
+  rover.setState({           // optional
+    position: [-400,-200,-200],
+    rotation: [0.4,0.3,0],
+    sensitivity: 0.1,
+    speed: 0.5
+  });
 }
 
 function draw() {
-  background(220);
+  background(0);
+  box(200);
 }
