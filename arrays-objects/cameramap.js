109caf4ccda518fca153da8ab1b75f0b8db36c17
0702849 <0702849@spsd.sk.ca>
2025-10-26 23:03:30 -0600

    couldnt create crosshair in 2d so i made it in 3d it just sits far enough away to look 2d

--- arrays-objects/cameramap.js
+++ arrays-objects/cameramap.js
@@ -48,12 +48,45 @@ function draw() {
   // boomStick();   ---  Create gun model that follows camera
 
   // -- HUD --
+
+  drawCrosshair({distance: 50,
+     size: 1,
+      color: [0, 250, 250], // - thought this might look better sorry if its confusing
+       occludable: true
+      });
+
+  
 }
 
 
-function drawCrosshair() {
+function drawCrosshair(options = {}) {
+  const {
+    distance = 50,
+    size = 2.5,
+    color = [0, 255, 255],
+    occludable = true,
+  } = options;
 
-  // use object to create crosshair settings
 
-  // dont forget to push and pop
+  //attempt to access current p5 camera
+  const cam = _renderer._curCamera;
+  const eye = createVector(cam.eyeX, cam.eyeY, cam.eyeZ);
+  const ctr = createVector(cam.centerX, cam.centerY, cam.centerZ);
+
+  //fwd direction
+  const fwd = p5.Vector.sub(ctr, eye).normalize();
+
+  //position of crosshair
+  const pos = p5.Vector.add(eye, p5.Vector.mult(fwd, distance));
+
+  push();
+  if (!occludable) hint(DISABLE_DEPTH_TEST);
+  translate(pos.x, pos.y, pos.z);
+
+  noStroke();
+  ambientMaterial(...color);
+  sphere(size, 12, 8);
+
+  if (!occludable) hint(ENABLE_DEPTH_TEST);
+  pop();
 }
