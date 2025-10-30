30e3d5c7e5ea4e14d000a2f77a1cf9c934210bcd
0702849 <0702849@spsd.sk.ca>
2025-10-30 09:25:51 -0600

    created grid based game

---
+++ grid-based-game/game.js
@@ -0,0 +1,16 @@
+// tile fight :p
+// Rylan Hamel Chan
+// October 30, 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
