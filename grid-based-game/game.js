7240d717d7f2c02dc70a0a7f4f6820f45c1eb4b0
0702849 <0702849@spsd.sk.ca>
2025-10-31 09:40:54 -0600

    Made the lawn function within the game.js file

--- grid-based-game/game.js
+++ grid-based-game/game.js
@@ -5,6 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// --constants--
+const SQUARE_DIMENSIONS = 9;
+
+// --global variables-
+let cellSize;
+let theLawn = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
 
 
 function setup() {
@@ -14,4 +25,55 @@ function setup() {
 function draw() {
   background(220);
   showLawn();
+}
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if (width < height) {
+    cellSize = width/SQUARE_DIMENSIONS;
+  }
+  else {
+    cellSize = height/SQUARE_DIMENSIONS;
+  }
+  // theGrid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
+}
+
+function draw() {
+  background(220);
+  showLawn();
+}
+
+
+function showLawn() {
+  for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
+    for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
+      if (theLawn[y][x] === 1) {
+        fill("black");
+      }
+      else if (theLawn[y][x] === 0) {
+        fill("green");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  if (theLawn[y][x] === 1) {
+    theLawn[y][x] = 0;
+  }
+  else if (theLawn[y][x] === 0) {
+    theLawn[y][x] = 1;
+  }
 }
